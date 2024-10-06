37af4fbeb33218b65d0897b459cbcb11a05effb7
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-06 14:10:10 -0400

    Initial Commit!!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="logo">She Codes Logo!!</h1>
+      </header>
+    </div>
+  );
+}

eab3ee293940dee08202359cc49a4b9071553e6a
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-09 17:22:35 -0400

    updating the api & meaning

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1 className="logo">She Codes Logo!!</h1>
+          <h1 className="logo">My Search Engine!!</h1>
             <main>
               <Dictionary />
             </main>
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,20 @@
 import React from "react";
 
 export default function Meaning(props) {
-    console.log(props.meaning);
+    console.log(props.meanings);
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
             <p>{props.meaning.definition}</p>
             <br />
-            <em><p>{props.meaning.synonyms}</p></em>
+            <em><p>{props.meaning.example}</p></em>
         </div>
-    );
-}
+                );
+            }
 
 
 
+        //  <p>{props.meaning.definition}</p>
+        //<p>{props.meaning.definition.example}</p>
 
-//  <p>{props.meaning.definition}</p>
-//<p>{props.meaning.definition.example}</p>
-
-//setResults(response.data.meanings[0].definition)
+        //setResults(response.data.meanings[0].definition)

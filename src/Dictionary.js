59101bdd1e4618a1610a7f974e38157c259345c3
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-09 16:44:03 -0400

    adding meanings to dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response);
+        console.log(response.data);
+        setResults(response.data)
     }
 
     function search(event) {
@@ -27,6 +30,7 @@ export default function Dictionary() {
                 <input type="search" 
                        onChange={handleKeywordChange} />
             </form>
+            <Results results={results} />
         </div>
     );
 }

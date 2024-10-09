3f8280c5251d16e27679831eb5b6de482e175090
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-09 14:12:59 -0400

    Applied the search engine to the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="logo">She Codes Logo!!</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="logo">She Codes Logo!!</h1>
+            <main>
+              <Dictionary />
+            </main>
+        </header>
+        <footer className="App-footer">
+          Coded by Rebecca Thombs
+        </footer>
+      </div>
     </div>
   );
 }

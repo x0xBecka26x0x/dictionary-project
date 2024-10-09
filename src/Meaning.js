3578e624fb39fe424ddfd816df39a2a53d8de24c
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-09 18:46:08 -0400

    put Synonyms in the project

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,26 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     console.log(props.meanings);
-    return (
-        <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            <p>{props.meaning.definition}</p>
-            <br />
-            <em><p>{props.meaning.example}</p></em>
-        </div>
-                );
-            }
+        return (
+            <div className="Meaning">
+                <h3>{props.meaning.partOfSpeech}</h3>
+                <br />
+                <strong>Definition: </strong> {props.meaning.definition}
+                <br />
+                <br />
+                <strong>Example: </strong> <em>{props.meaning.example}</em>
+                <br />
+                <br />
+                <strong>Synonyms: </strong>
+                <Synonyms synonyms={props.meaning.synonyms} />
+          </div>
+        );
+     }
 
 
 
         //  <p>{props.meaning.definition}</p>
         //<p>{props.meaning.definition.example}</p>
-
         //setResults(response.data.meanings[0].definition)

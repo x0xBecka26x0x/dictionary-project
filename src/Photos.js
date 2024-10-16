5b1b4c93e62f10b662849fbbfdbc12d731cd242d
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-16 17:12:05 -0400

    putting in photos

--- src/Photos.js
+++ src/Photos.js
@@ -5,17 +5,15 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <div className="row">
-            {props.photos.map(function (photo, index) {
-              return (
-                <div className="col-3" key={index}>
-                  <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img src={photo.src.landscape} className="img-fluid" />
-                  </a>
-                </div>
-              )
+        <div className="row-3">
+              {props.photos.map(function (photo, index) {
+                return (
+                  <div className="col-3" key={index}>
+                      <img src={photo.src.landscape} className="img-fluid" />
+                  </div>
+              );
             })}
-        </div>
+           </div>
       </section>
     );
   } else {

c1554e586bce064c203813ff91805984303be231
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-16 17:38:23 -0400

    finalizing project

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -7,7 +7,7 @@ export default function Phonetic(props) {
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <span className="text">{props.phonetic.text}</span>
+      <span className="text">{props.phonetic}</span>
     </div>
   );
 }

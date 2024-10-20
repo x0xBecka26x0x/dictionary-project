cc4c69ae895b44ef6e6a7ced83d293ca9d16b384
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-10-20 15:03:09 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1 className="logo">My Dictionary 📖 </h1>
+          <h1 className="logo"> My Dictionary 📖 </h1>
             <main>
               <Dictionary defaultKeyword="sunset" />
             </main>
